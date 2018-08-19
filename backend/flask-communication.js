e60a3eeab7b2214c89c1e16dad37df4a688c5e16
Michael Vann <mvann@Michaels-iMac.local>
2018-08-19 12:59:58 -0700

    connected to flask

--- backend/flask-communication.js
+++ backend/flask-communication.js
@@ -1,11 +1,16 @@
 const request = require('request');
+const fs = require('fs');
 
-var formData = {
-
+module.exports.uploadFileToRepo = function(filePath) {
+  var formData = {
+    file: fs.createReadStream(filePath)
+  }
+  request.post({
+    url:'http://localhost:5000',
+    formData: formData,
+  }, (err, response, body) => {
+    console.log("Error:", err);
+    console.log("Response:", response);
+    console.log("Body:", body);
+  });
 }
-request.post({
-  url:'http://localhost:5000',
-  formData: formData,
-}, (err, response, body) => {
-  
-});
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./api/api');
+const repoConnection = require('./flask-communication');
 
 const port = 5001;
 
 app.use('/api', apiRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+repoConnection.uploadFileToRepo(__dirname + '/test.deb');
