e1dc6e2ce89b3687cd7f4be9cbd95ef3df91041f
mvann <michaelmvann@gmail.com>
2018-08-26 20:54:57 -0700

    delete files

---
+++ backend/deleteFiles.js
@@ -0,0 +1,13 @@
+const db = require('./api/db').connect;
+const mongoose = require('mongoose');
+require('./package/packageModel');
+const Package = mongoose.model('Packages');
+
+db.once('open', function(){
+  Package.deleteMany({}, (err) => {
+    if (err)
+      console.log('delete many err:', err);
+    else
+      console.log('deleted packages');
+  });
+});
